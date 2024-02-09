e7beb06325b2edffb3a5f4b8ee2e50db7f4bfc8d
Adithi Ramesh <adithi.ramesh02@gmail.com>
2024-02-08 17:42:56 -0800

    Fixed.jpgG MIME test case

--- server.c
+++ server.c
@@ -298,10 +298,11 @@ void serve_local_file(int client_socket, const char *path) {
             char *file_content = (char*)malloc(file_size);
             fseek(fp, 0, SEEK_SET);
             fread(file_content, file_size, 1, fp);
+            file_content[file_size] = '\0';
             fclose(fp);
-            snprintf(response,  100 + file_size, "HTTP/1.0 200 OK\r\nContent-Type: image/jpg\r\nContent-Length: %ld\r\n\r\n", file_size); 
+            snprintf(response,  100 + file_size, "HTTP/1.0 200 OK\r\nContent-Type: image/jpeg\r\nContent-Length: %ld\r\n\r\n", file_size + 1); 
             send(client_socket, response, strlen(response), 0);
-            send(client_socket, file_content, file_size, 0);     
+            send(client_socket, file_content, file_size + 1, 0);     
             free(file_content);
         }
     }   
